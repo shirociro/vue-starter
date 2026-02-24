6ca168f72ddc0caf28a09af33bef2fc998c51a75
rico.carbonel@motivit.com <rico.carbonel@motivit.com>
2026-02-25 01:22:47 +0800

    update axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Create an Axios instance
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000", // your API URL
+//   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000", 
+  baseURL: "https://dotnet-starter.onrender.com", 
   headers: {
     "Content-Type": "application/json",
   },
